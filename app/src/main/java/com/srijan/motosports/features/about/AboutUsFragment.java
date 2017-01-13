97dbf8370a94ed4b6717b35eebc4a4152d2495a0
Mani-Itachi <manikantajreinstein97@gmail.com>
2017-01-13 21:58:49 +0530

    Add about us and models feature

--- app/src/main/java/com/srijan/motosports/features/about/AboutUsFragment.java
+++ app/src/main/java/com/srijan/motosports/features/about/AboutUsFragment.java
@@ -20,10 +20,11 @@ public class AboutUsFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment.
         return inflater.inflate(R.layout.fragment_about_us, container, false);
     }
 
